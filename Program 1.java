3d7f68192674d35083b0de9b37b7ab909cf619b9
sandeshagarwal <sandeshagarwal10@gmail.com>
2020-04-01 09:42:17 +0530

    P1 - Linear search in an Array (Commit 3)

--- Program 1.java
+++ Program 1.java
@@ -12,7 +12,11 @@ import java.util.*;
     }
     //System.out.print("hii"); I want to ask why the code is not coming here at return -1 when there is a no. present in the array
     //return -1 is outside for loop and not in else condition then why the control is not coming here when there is a no. present in the array
-    return -1;
+   // It does not comes here becuse it will not exceute the statements after it will find the first return(ie.return control to the method where it is called)
+   // If we are using else statement after if, and it contains return -1, It will print -1 because 
+   //if you do not query the first no. in the array then if statement is false and will go else condition and return -1 from there and 
+   //then do not goes to any reurn statement after that.
+   return -1;
   }
  }
   public class Main{
