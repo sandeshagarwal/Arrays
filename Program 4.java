7440bfc31d5eaf7b7d6a89e4a4a5969115630d39
sandeshagarwal <sandeshagarwal10@gmail.com>
2020-04-20 22:14:24 +0530

    P4 - Inserting the element a specific position
    Here, we are just replacing the element in the array unlike the P3 where we are keeping the element whose position where the new no. was assigned by creating an entirely new Arrayimport java.util.*;
    public class Main{
    public static void main(String args[]){
    Scanner sc = new Scanner(System.in);
    System.out.println("Enter the number of elements in the Array");
    int n = sc.nextInt();
    int arr[] = new int[n];
    System.out.println("Enter the elements in the Array");
    for(int i=0;i<n;i++)
      arr[i] = sc.nextInt();
    System.out.println("Enter the element to be Inserted in Array");
    int x = sc.nextInt();
    System.out.println("Enter the position where element is to be Inserted in Array");
    int pos = sc.nextInt();
    System.out.println("The Initial Array is");
    System.out.println(Arrays.toString(arr));
    arr[pos-1] = x;
    System.out.println("The Final Array is");
    System.out.println(Arrays.toString(arr));
    
    }
    }

---
+++ Program 4.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main{
+public static void main(String args[]){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the number of elements in the Array");
+int n = sc.nextInt();
+int arr[] = new int[n];
+System.out.println("Enter the elements in the Array");
+for(int i=0;i<n;i++)
+  arr[i] = sc.nextInt();
+System.out.println("Enter the element to be Inserted in Array");
+int x = sc.nextInt();
+System.out.println("Enter the position where element is to be Inserted in Array");
+int pos = sc.nextInt();
+System.out.println("The Initial Array is");
+System.out.println(Arrays.toString(arr));
+arr[pos-1] = x;
+System.out.println("The Final Array is");
+System.out.println(Arrays.toString(arr));
+
+}
+}
